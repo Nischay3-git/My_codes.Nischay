1c1905b7e9663fc5f420792735cd02a4d3fb9e41
NISCHAY CHHETRI <chhetrinischay3@gmail.com>
2025-08-21 04:07:55 +0530

    Add files via upload

---
+++ leap_year.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int y;
+    printf("Enter the year: ");
+    scanf("%d", &y);
+//century years are leap year only when they can be divided by 400
+    if (y%4==0 && y%100!=0 || y%400==0)
+    {
+        printf("LEAP YEAR");
+    }
+
+    else
+    {
+        printf("Not leap year");
+    }
+    return 0;
+}
