faca36e7b394a60a38c8ba0e9ac0ddaf9e386927
NISCHAY CHHETRI <chhetrinischay3@gmail.com>
2025-08-21 04:00:14 +0530

    Add files via upload

---
+++ leap_year.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int y;
+    printf("Enter the year: ");
+    scanf("%d", &y);
+
+    if (y % 4 == 0)
+    {
+        printf("LEAP YEAR");
+    }
+    
+    else
+    {
+        printf("Not leap year");
+    }
+    return 0;
+}
